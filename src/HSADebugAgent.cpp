ROCm/rocr_debug_agent

commit dbceae505e4dcfe41f6b8850aa2bf11c561bfaa9

Remove the dependency on hsa.h in HSADebugAgentGDBInterface.h
Change-Id: Id15c125a4656b4acce2301bc796b9da10ef4a5f4

src/HSADebugAgent.cpp
@@ -373,7 +373,7 @@ static hsa_status_t QueryAgentCallback(hsa_agent_t agent, void *pData)
     GPUAgentInfo *pGpuAgent = new GPUAgentInfo;
     memset(pGpuAgent, 0, sizeof(GPUAgentInfo));
 
-    pGpuAgent->agent = agent;
+    pGpuAgent->agent = reinterpret_cast<void*>(agent.handle);
     pGpuAgent->pQueueList = nullptr;
     pGpuAgent->agentStatus = AGENT_STATUS_UNSUPPORTED;
 
@@ -574,6 +574,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
         uint64_t kernelCodeAddress = 0;
         DebugTrapBuff* pTrapHandlerBuffer = nullptr;
 
+        hsa_agent_t& agent = *reinterpret_cast<hsa_agent_s*>(&pAgent->agent);
         hsa_region_t kernargSegment = {0};
         hsa_executable_symbol_t symbol = {0};
         hsa_status_t status = HSA_STATUS_SUCCESS;
@@ -607,7 +608,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
 
         // Load debug trap handler code object
         status = gs_OrigCoreApiTable.hsa_executable_load_agent_code_object_fn(
-                debugTrapHandlerExecutable, pAgent->agent, debugTrapHandlerCodeObjectReader, NULL, NULL);
+                debugTrapHandlerExecutable, agent, debugTrapHandlerCodeObjectReader, NULL, NULL);
         if (status != HSA_STATUS_SUCCESS)
         {
             AGENT_ERROR("Cannot load debug trap handler code object.");
@@ -624,7 +625,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
 
         // find the kernel symbol.
         status = gs_OrigCoreApiTable.hsa_executable_get_symbol_by_name_fn(
-                debugTrapHandlerExecutable, pEntryPointName, &(pAgent->agent), &symbol);
+                debugTrapHandlerExecutable, pEntryPointName, &(agent), &symbol);
         if (status != HSA_STATUS_SUCCESS)
         {
             AGENT_ERROR("Cannot find debug trap handler symbol.");
@@ -652,7 +653,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
         }
 
         status = gs_OrigCoreApiTable.hsa_signal_create_fn(
-                0, 0, &(pAgent->agent), &debugTrapSignal);
+                0, 0, &(agent), &debugTrapSignal);
         if (status != HSA_STATUS_SUCCESS)
         {
             AGENT_ERROR("Cannot create debug event signal.");
@@ -663,7 +664,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
         // TODO: put the trap buffer in device memory for efficiency
         //       use the copy APIs to update the value
         status = gs_OrigCoreApiTable.hsa_agent_iterate_regions_fn(
-                pAgent->agent, FindKernargSegment, &kernargSegment);
+                agent, FindKernargSegment, &kernargSegment);
         if (!kernargSegment.handle | (status != HSA_STATUS_SUCCESS)) {
             AGENT_ERROR("Cannot find kernarg segment for trap buffer.");
             return DEBUG_AGENT_STATUS_FAILURE;

src/HSADebugInfo.cpp
@@ -206,12 +206,14 @@ DebugAgentStatus PreemptAgentQueues(GPUAgentInfo* pAgent)
     QueueInfo *pQueue = pAgent->pQueueList;
     while (pQueue != nullptr)
     {
+        hsa_queue_t* queue = reinterpret_cast<hsa_queue_t*>(pQueue->queue);
+
         // preempt the queue
-        kmt_status = hsaKmtUpdateQueue(pQueue->queue->id,
+        kmt_status = hsaKmtUpdateQueue(queue->id,
                                        0,
                                        HSA_QUEUE_PRIORITY_NORMAL,
                                        NULL,
-                                       pQueue->queue->size,
+                                       queue->size,
                                        NULL);
         if (kmt_status != HSAKMT_STATUS_SUCCESS)
         {
@@ -256,16 +258,18 @@ DebugAgentStatus ResumeAgentQueues(GPUAgentInfo* pAgent)
     QueueInfo *pQueue = pAgent->pQueueList;
     while (pQueue != nullptr)
     {
+        hsa_queue_t* queue = reinterpret_cast<hsa_queue_t*>(pQueue->queue);
+
         if (pQueue->queueStatus != HSA_STATUS_SUCCESS)
         {
             break;
         }
 
-        kmt_status = hsaKmtUpdateQueue(pQueue->queue->id,
+        kmt_status = hsaKmtUpdateQueue(queue->id,
                                        100,
                                        HSA_QUEUE_PRIORITY_NORMAL,
-                                       pQueue->queue->base_address,
-                                       pQueue->queue->size,
+                                       queue->base_address,
+                                       queue->size,
                                        NULL);
         if (kmt_status != HSAKMT_STATUS_SUCCESS)
         {
@@ -512,12 +516,12 @@ GPUAgentInfo *GetAgentFromList(uint32_t nodeId)
     return nullptr;
 }
 
-GPUAgentInfo *GetAgentFromList(hsa_agent_t agentHandle)
+GPUAgentInfo *GetAgentFromList(void* agentHandle)
 {
     GPUAgentInfo *pList = _r_rocm_debug_info.pAgentList;
     while (pList != nullptr)
     {
-        if (pList->agent.handle == agentHandle.handle)
+        if (pList->agent == agentHandle)
         {
             return pList;
         }

src/HSAHandleMemoryFault.cpp
@@ -68,7 +68,7 @@ HSADebugAgentHandleMemoryFault(hsa_amd_event_t event, void* pData)
         std::lock_guard<std::mutex> lock(debugAgentAccessLock);
 
         DebugAgentStatus status = DEBUG_AGENT_STATUS_SUCCESS;
-        GPUAgentInfo* pAgent = GetAgentFromList(event.memory_fault.agent);
+        GPUAgentInfo* pAgent = GetAgentFromList(reinterpret_cast<void*>(event.memory_fault.agent.handle));
 
         DebugAgentEventInfo *pEventInfo = _r_rocm_debug_info.pDebugAgentEvent;
         if (pEventInfo == nullptr)

src/HSAHandleQueueError.cpp
@@ -95,7 +95,10 @@ void HSADebugAgentHandleQueueError(hsa_status_t status, hsa_queue_t* pHsaQueueT,
     // Call the original callback registered when create queue in runtime
     if (pQueue->callback != nullptr)
     {
-        pQueue->callback(status, pHsaQueueT, pQueue->data);
+        auto* callback
+            = reinterpret_cast<void (*)(hsa_status_t, hsa_queue_t*, void*)>(pQueue->callback);
+
+        (*callback)(status, pHsaQueueT, pQueue->data);
     }
 }
 

src/HSAIntercept.cpp
@@ -205,7 +205,7 @@ HsaDebugAgentHsaQueueCreate(
 
     QueueInfo* pNewQueueInfo = new QueueInfo;
     pNewQueueInfo->queueStatus = HSA_STATUS_SUCCESS;
-    pNewQueueInfo->callback = callback;
+    pNewQueueInfo->callback = reinterpret_cast<void*>(callback);
     pNewQueueInfo->data = data;
     pNewQueueInfo->pWaveList = nullptr;
     pNewQueueInfo->pPrev = nullptr;
@@ -533,7 +533,7 @@ AddCodeObjectInfoCallback(
         return status;
     }
 
-    GPUAgentInfo *pAgent = GetAgentFromList(loadedAgent);
+    GPUAgentInfo *pAgent = GetAgentFromList(reinterpret_cast<void*>(loadedAgent.handle));
     ((ExecutableInfo *)data)->nodeId = pAgent->nodeId;
 
     CodeObjectInfo* pList = new CodeObjectInfo;

src/Include/HSADebugAgentGDBInterface.h
@@ -41,8 +41,6 @@
 // Max breakpoint number based on the trap buffer size (0x1000)
 #define AGENT_MAX_BREAKPOINT 510
 
-#include "hsa.h"
-
 #pragma pack(push,1)
 extern "C" {
 
@@ -163,11 +161,11 @@ typedef struct _QueueInfo
     // Queue status. Value is QueueStatus. TODO: hold runtime queue error state
     uint64_t queueStatus;
     // Pointer to the queue.
-    hsa_queue_t* queue;
+    void*    queue;
     // Runtime queue id.
     uint64_t queueId;
     // Orignal callback registered by the HSA runtime for asynchronous event.
-    void (*callback)(hsa_status_t status, hsa_queue_t* source, void* data);
+    void* callback;
     // Orignal application data that is passed to the callback.
     void* data;
     // Context save area
@@ -192,7 +190,7 @@ typedef struct _GPUAgentInfo
     // Agent status. Vaule is AgentStatus.
     uint64_t agentStatus;
     // GPU Agent handle.
-    hsa_agent_t agent;
+    void*    agent;
     // Agent node id.
     uint32_t nodeId;
     // Agent name.

src/Include/HSADebugInfo.h
@@ -78,7 +78,7 @@ void PrintWaves(GPUAgentInfo* pAgent, std::map<uint64_t, std::pair<uint64_t, Wav
 GPUAgentInfo* GetAgentFromList(uint32_t node);
 
 // Get the pointer of specified agent info by agent handle from the agent link list.
-GPUAgentInfo* GetAgentFromList(hsa_agent_t agentHandle);
+GPUAgentInfo* GetAgentFromList(void* agentHandle);
 
 // Get the pointer of specified agent info by a queue id of it.
 GPUAgentInfo* GetAgentByQueueID(uint64_t queueId);

commit ff8b80602ae34ca1e909a9022f0f01246256f3a8

Fix a build error caused by commit dbceae505e4dcfe41f6b8850aa2bf11c561bfaa9.
Change-Id: I729d51815e2d0849a9999452b1c414729d3bb7cf

src/HSADebugAgent.cpp
@@ -373,7 +373,7 @@ static hsa_status_t QueryAgentCallback(hsa_agent_t agent, void *pData)
     GPUAgentInfo *pGpuAgent = new GPUAgentInfo;
     memset(pGpuAgent, 0, sizeof(GPUAgentInfo));
 
-    pGpuAgent->agent = agent;
+    pGpuAgent->agent = reinterpret_cast<void*>(agent.handle);
     pGpuAgent->pQueueList = nullptr;
     pGpuAgent->agentStatus = AGENT_STATUS_UNSUPPORTED;
 
@@ -574,6 +574,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
         uint64_t kernelCodeAddress = 0;
         DebugTrapBuff* pTrapHandlerBuffer = nullptr;
 
+        hsa_agent_t agent = { reinterpret_cast<decltype(hsa_agent_s::handle)>(pAgent->agent) };
         hsa_region_t kernargSegment = {0};
         hsa_executable_symbol_t symbol = {0};
         hsa_status_t status = HSA_STATUS_SUCCESS;
@@ -607,7 +608,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
 
         // Load debug trap handler code object
         status = gs_OrigCoreApiTable.hsa_executable_load_agent_code_object_fn(
-                debugTrapHandlerExecutable, pAgent->agent, debugTrapHandlerCodeObjectReader, NULL, NULL);
+                debugTrapHandlerExecutable, agent, debugTrapHandlerCodeObjectReader, NULL, NULL);
         if (status != HSA_STATUS_SUCCESS)
         {
             AGENT_ERROR("Cannot load debug trap handler code object.");
@@ -624,7 +625,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
 
         // find the kernel symbol.
         status = gs_OrigCoreApiTable.hsa_executable_get_symbol_by_name_fn(
-                debugTrapHandlerExecutable, pEntryPointName, &(pAgent->agent), &symbol);
+                debugTrapHandlerExecutable, pEntryPointName, &(agent), &symbol);
         if (status != HSA_STATUS_SUCCESS)
         {
             AGENT_ERROR("Cannot find debug trap handler symbol.");
@@ -652,7 +653,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
         }
 
         status = gs_OrigCoreApiTable.hsa_signal_create_fn(
-                0, 0, &(pAgent->agent), &debugTrapSignal);
+                0, 0, &(agent), &debugTrapSignal);
         if (status != HSA_STATUS_SUCCESS)
         {
             AGENT_ERROR("Cannot create debug event signal.");
@@ -663,7 +664,7 @@ static DebugAgentStatus AgentSetDebugTrapHandler()
         // TODO: put the trap buffer in device memory for efficiency
         //       use the copy APIs to update the value
         status = gs_OrigCoreApiTable.hsa_agent_iterate_regions_fn(
-                pAgent->agent, FindKernargSegment, &kernargSegment);
+                agent, FindKernargSegment, &kernargSegment);
         if (!kernargSegment.handle | (status != HSA_STATUS_SUCCESS)) {
             AGENT_ERROR("Cannot find kernarg segment for trap buffer.");
             return DEBUG_AGENT_STATUS_FAILURE;

src/HSADebugInfo.cpp
@@ -211,12 +211,14 @@ DebugAgentStatus PreemptAgentQueues(GPUAgentInfo* pAgent)
     QueueInfo *pQueue = pAgent->pQueueList;
     while (pQueue != nullptr)
     {
+        hsa_queue_t* queue = reinterpret_cast<hsa_queue_t*>(pQueue->queue);
+
         // preempt the queue
-        kmt_status = hsaKmtUpdateQueue(pQueue->queue->id,
+        kmt_status = hsaKmtUpdateQueue(queue->id,
                                        0,
                                        HSA_QUEUE_PRIORITY_NORMAL,
                                        NULL,
-                                       pQueue->queue->size,
+                                       queue->size,
                                        NULL);
         if (kmt_status != HSAKMT_STATUS_SUCCESS)
         {
@@ -261,16 +263,18 @@ DebugAgentStatus ResumeAgentQueues(GPUAgentInfo* pAgent)
     QueueInfo *pQueue = pAgent->pQueueList;
     while (pQueue != nullptr)
     {
+        hsa_queue_t* queue = reinterpret_cast<hsa_queue_t*>(pQueue->queue);
+
         if (pQueue->queueStatus != HSA_STATUS_SUCCESS)
         {
             break;
         }
 
-        kmt_status = hsaKmtUpdateQueue(pQueue->queue->id,
+        kmt_status = hsaKmtUpdateQueue(queue->id,
                                        100,
                                        HSA_QUEUE_PRIORITY_NORMAL,
-                                       pQueue->queue->base_address,
-                                       pQueue->queue->size,
+                                       queue->base_address,
+                                       queue->size,
                                        NULL);
         if (kmt_status != HSAKMT_STATUS_SUCCESS)
         {
@@ -517,12 +521,12 @@ GPUAgentInfo *GetAgentFromList(uint32_t nodeId)
     return nullptr;
 }
 
-GPUAgentInfo *GetAgentFromList(hsa_agent_t agentHandle)
+GPUAgentInfo *GetAgentFromList(void* agentHandle)
 {
     GPUAgentInfo *pList = _r_rocm_debug_info.pAgentList;
     while (pList != nullptr)
     {
-        if (pList->agent.handle == agentHandle.handle)
+        if (pList->agent == agentHandle)
         {
             return pList;
         }

src/HSAHandleMemoryFault.cpp
@@ -68,7 +68,7 @@ HSADebugAgentHandleMemoryFault(hsa_amd_event_t event, void* pData)
         std::lock_guard<std::mutex> lock(debugAgentAccessLock);
 
         DebugAgentStatus status = DEBUG_AGENT_STATUS_SUCCESS;
-        GPUAgentInfo* pAgent = GetAgentFromList(event.memory_fault.agent);
+        GPUAgentInfo* pAgent = GetAgentFromList(reinterpret_cast<void*>(event.memory_fault.agent.handle));
 
         DebugAgentEventInfo *pEventInfo = _r_rocm_debug_info.pDebugAgentEvent;
         if (pEventInfo == nullptr)

src/HSAHandleQueueError.cpp
@@ -95,7 +95,10 @@ void HSADebugAgentHandleQueueError(hsa_status_t status, hsa_queue_t* pHsaQueueT,
     // Call the original callback registered when create queue in runtime
     if (pQueue->callback != nullptr)
     {
-        pQueue->callback(status, pHsaQueueT, pQueue->data);
+        auto* callback
+            = reinterpret_cast<void (*)(hsa_status_t, hsa_queue_t*, void*)>(pQueue->callback);
+
+        (*callback)(status, pHsaQueueT, pQueue->data);
     }
 }
 

src/HSAIntercept.cpp
@@ -208,7 +208,7 @@ HsaDebugAgentHsaQueueCreate(
 
     QueueInfo* pNewQueueInfo = new QueueInfo;
     pNewQueueInfo->queueStatus = HSA_STATUS_SUCCESS;
-    pNewQueueInfo->callback = callback;
+    pNewQueueInfo->callback = reinterpret_cast<void*>(callback);
     pNewQueueInfo->data = data;
     pNewQueueInfo->pWaveList = nullptr;
     pNewQueueInfo->pPrev = nullptr;
@@ -660,7 +660,7 @@ AddCodeObjectInfoCallback(
         return status;
     }
 
-    GPUAgentInfo *pAgent = GetAgentFromList(loadedAgent);
+    GPUAgentInfo *pAgent = GetAgentFromList(reinterpret_cast<void*>(loadedAgent.handle));
     ((ExecutableInfo *)data)->nodeId = pAgent->nodeId;
 
     CodeObjectInfo* pList = new CodeObjectInfo;

src/Include/HSADebugAgentGDBInterface.h
@@ -41,8 +41,6 @@
 // Max breakpoint number based on the trap buffer size (0x1000)
 #define AGENT_MAX_BREAKPOINT 510
 
-#include "hsa.h"
-
 #pragma pack(push,1)
 extern "C" {
 
@@ -163,13 +161,13 @@ typedef struct _QueueInfo
     // Queue status. Value is QueueStatus. TODO: hold runtime queue error state
     uint64_t queueStatus;
     // Pointer to the queue.
-    hsa_queue_t* queue;
+    void*    queue;
     // Runtime queue id.
     uint64_t queueId;
     // Agent node id the queue belongs to.
     uint32_t nodeId;
     // Orignal callback registered by the HSA runtime for asynchronous event.
-    void (*callback)(hsa_status_t status, hsa_queue_t* source, void* data);
+    void* callback;
     // Orignal application data that is passed to the callback.
     void* data;
     // Context save area
@@ -194,7 +192,7 @@ typedef struct _GPUAgentInfo
     // Agent status. Vaule is AgentStatus.
     uint64_t agentStatus;
     // GPU Agent handle.
-    hsa_agent_t agent;
+    void*    agent;
     // Agent node id.
     uint32_t nodeId;
     // Agent name.

src/Include/HSADebugInfo.h
@@ -78,7 +78,7 @@ void PrintWaves(GPUAgentInfo* pAgent, std::map<uint64_t, std::pair<uint64_t, Wav
 GPUAgentInfo* GetAgentFromList(uint32_t node);
 
 // Get the pointer of specified agent info by agent handle from the agent link list.
-GPUAgentInfo* GetAgentFromList(hsa_agent_t agentHandle);
+GPUAgentInfo* GetAgentFromList(void* agentHandle);
 
 // Get the pointer of specified agent info by a queue id of it.
 GPUAgentInfo* GetAgentByQueueID(uint64_t queueId);